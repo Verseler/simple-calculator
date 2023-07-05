b33a007add40bf93482e0962a96cbfe6c267f98b
Verseler <handuman.verselerkerr@gmail.com>
2023-07-05 17:41:04 +0800

    fix issues with zero,  result overflow

--- script.js
+++ script.js
@@ -1,104 +1,116 @@
-
 const OPERATORS = ['÷', '×', '+', '−'];
 const DEFAULT_VALUE = '0';
-const totalResult = document.getElementById('total-amount');
-let expression = ''; //expression value during js computation
-const expressionDisplay = document.getElementById('expression'); //expression value displayed in the GUI
+const resultOutput = document.getElementById('result'); //result displayed in the GUI
+let result = ''; //result variable during js computation
 
-//set expression when button is clicked in the calculator
+
+//set result when clicked a button in the calculator GUI
 const buttons = document.getElementsByClassName('button');
 Array.from(buttons).forEach(button => {
     button.addEventListener('click', () => {
         let buttonValue = button.innerText;
 
         if (buttonValue === 'CLEAR') {
-            expression = clearAllChar(expression);
+            result = clearAllResult(result);
         }
         else if (buttonValue === 'DELETE') {
-            expression = deleteLastChar(expression);
+            result = popResult(result);
         }
         else {
             //else the button value is either number or an operator
-            expression = addToTheExpression(buttonValue, expression);
+            result = addToTheResult(buttonValue, result);
         }
 
-        //then update the old expression value displayed in calculator with new expression value
-        updateExpression(expression);
+        //then update the old result displayed in calculator with new result
+        updateResult(result);
     });
 });
 
 
-/* FUNCTIONS FOR EXPRESSION */
-//delete the last character of the expression
-function deleteLastChar(oldExpression) {
-    return (oldExpression.slice(-1) === " ") ? oldExpression.slice(0, -3) : oldExpression.slice(0, -1);
+//delete the last character in the result
+function popResult(oldResult) {
+    return (oldResult.slice(-1) === " ") ? oldResult.slice(0, -3) : oldResult.slice(0, -1);
 }
 
-//delete all characters in the expression
-function clearAllChar(expression) {
-    clearTotalAmount()
-    return expression.innerText = DEFAULT_VALUE;
+//delete all characters in the result
+function clearAllResult(result) {
+    return result.innerText = DEFAULT_VALUE;
 }
 
-//add the button value to the expression
-function addToTheExpression(value, oldExpression) {
-    let operator = oldExpression + " " + value + " ";
-    let number = oldExpression + value;
-    //add value to the expression if it is an operator or a number
-    return (OPERATORS.includes(value)) ? operator : number;
+//add the button value to the result
+function addToTheResult(value, oldResult) {
+    //remove the defaul value zero when enter a number or operator
+    if(oldResult === '0') {
+        oldResult = "";
+    }
+
+    //if the value is an operator add a space around the operator
+    return (OPERATORS.includes(value)) ? `${oldResult} ${value} ` : oldResult + value;
 }
 
-// update the displayed expression in the calculator
-function updateExpression(newExpression) {
-    //update expression
-    expression = newExpression;
-    //update expression display in the calculator GUI
-    expressionDisplay.innerText = expression;
+//update the displayed result output in the calculator GUI
+function updateResult(newResult) {
+    result = newResult;
+    //update result output that is displayed in the calculator GUI
+    resultOutput.innerText = result;
+
+    //adjust the result output size depend on its size
+    let resultSize = resultOutput.innerText.length;
+    if(resultSize >= 15) {
+        resultOutput.style.fontSize = '1.5rem';
+        resultOutput.style.bottom = '5px';
+    }
+    else {
+        resultOutput.style.fontSize = '2.5rem';
+        resultOutput.style.bottom = '20px';
+    }
 }
 
-//compute the current given expression
-function getEqual() {
-    CURRENT_NUM_ARRAY = expression.split(' ');
-    console.log(CURRENT_NUM_ARRAY);
-    findFirstSetOfNums();
-}
 
 /* find the FIRST set of numbers that will be computed based on bidmas rule */
-function findFirstSetOfNums() {
+function getEqual() {
+    //remove spaces in the result then add all characters to CURRENT_RESULT array
+    CURRENT_RESULT = result.split(' ');
+    console.log(CURRENT_RESULT); //for debug purposes
+
     //first loop will execute the inner loop based on the current operator
+    //so it compute first the divison, then multiplication, add, and subtract operator based on bidmas rule
     OPERATORS.forEach((operator) => {
-        //second loop will check if current operator exist in the currentNumArr array
-        while (CURRENT_NUM_ARRAY.includes(operator)) {
+        //second loop will check if current operator exist in the CURRENT_RESULT array
+        //if not proceed to the next operator if yes compute that set of numbers with the current operator
+        while (CURRENT_RESULT.includes(operator)) {
             //get the set of numbers to be computed based on the currentOperatorIndex
-            let currentOperatorIndex = CURRENT_NUM_ARRAY.indexOf(operator);
-            operate(currentOperatorIndex);
+            let currentSetOfExpressionOperatorIndex = CURRENT_RESULT.indexOf(operator);
+            compute(currentSetOfExpressionOperatorIndex, CURRENT_RESULT);
         }
     });
+
+    //update all expression the result is computed display the totalResult
+    let totalResult = CURRENT_RESULT[0].toString();
+    updateResult(totalResult);
+    console.log(CURRENT_RESULT); //for debugging purposes
 }
 
-function operate(operatorIndex) {
-    const currentSetOfNums = [];
-    let setOfNumsStartingIndex = operatorIndex - 1;
-    let setOfNumsEndingIndex = operatorIndex + 2;
+//set the first set of numbers to be computed based on bidmas rule
+function compute(operatorIndex) {
+    const CURRENT_SET_OF_EXPRESSION = [];
+    let setOfExpressionStartingIndex = operatorIndex - 1;
+    let setOfExpressionEndingIndex = operatorIndex + 2;
 
     //Set the selected set of numbers that to be computed
-    for (let i = setOfNumsStartingIndex; i !== setOfNumsEndingIndex; i++) {
-        currentSetOfNums.push(CURRENT_NUM_ARRAY[i]);
+    for (let indx = setOfExpressionStartingIndex; indx !== setOfExpressionEndingIndex; indx++) {
+        CURRENT_SET_OF_EXPRESSION.push(CURRENT_RESULT[indx]);
     }
-    let [firstNum, operator, secondNum] = currentSetOfNums;
-    let operationResult = compute(firstNum, operator, secondNum);
-    //update the currentNumArray with computed results of the selected set of numbers
-    CURRENT_NUM_ARRAY.splice(setOfNumsStartingIndex, 3, `${operationResult}`);
-
-    let totalResult = Number(CURRENT_NUM_ARRAY[0]);
-    updateTotalAmount(totalResult);
-    console.log(CURRENT_NUM_ARRAY); //!!!!temporary for debugging
+    let [firstNum, operator, secondNum] = CURRENT_SET_OF_EXPRESSION;
+    //calculate the first and second number with its operator of the current set of expression
+    let operationResult = calculate(firstNum, operator, secondNum);
+    //update the CURRENT_RESULT with computed results of the selected set of numbers
+    CURRENT_RESULT.splice(setOfExpressionStartingIndex, 3, `${operationResult}`);
 }
 
 
-/* caculate numbers based on the given operator */
-function compute(firstNum, operator, secondNum) {
-    console.log('compute');
+/* caculate two numbers based on the given operator */
+function calculate(firstNum, operator, secondNum) {
     firstNum = Number(firstNum);
     secondNum = Number(secondNum);
 
@@ -116,23 +128,8 @@ function compute(firstNum, operator, secondNum) {
     }
 }
 
-function updateTotalAmount(results) {
-    let totalResultValue = Number(totalResult.innerText) + results;
-    totalResult.innerText = totalResultValue;
-
-    //update expressionDisplay
-    updateExpression(totalResultValue);
-}
-
-
-function clearTotalAmount() {
-    totalResult.innerText = DEFAULT_VALUE;
-}
-
-
 
 window.onload = () => {
-    clearTotalAmount(DEFAULT_VALUE)
-    updateExpression(DEFAULT_VALUE);
+    updateResult(DEFAULT_VALUE);
 }
 
